dc3cc8fb3645cf219ddfc3736092740864f57ea5
zainabMohdR <2120007606@uod.edu.sa>
2016-03-12 17:53:06 +0300

    added functionality to login and signup buttons in welcome.xml

--- app/src/main/java/com/test/wichat/seniorproject/WelcomeActivity.java
+++ app/src/main/java/com/test/wichat/seniorproject/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package com.test.wichat.seniorproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button loginBtn = (Button)findViewById(R.id.btnLogin);
+        Button signupBtn = (Button)findViewById(R.id.btnSignup);
+
+
+        loginBtn.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent signinIntent = new Intent(WelcomeActivity.this, login.class);
+                        startActivity(signinIntent);
+
+                    }
+                }
+        );
+
+        signupBtn.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent signupIntent = new Intent(WelcomeActivity.this, register.class);
+                        startActivity(signupIntent);
+
+                    }
+                }
+        );
     }
+
 }
